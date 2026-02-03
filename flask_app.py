17b0a29403bfd05edd2331bb6dad1988044bb275
Jakispaki <jakob.steuri@bluewin.ch>
2026-02-03 18:09:03 +0000

    bro

--- flask_app.py
+++ flask_app.py
@@ -61,7 +61,7 @@ def login():
 
         if user:
             login_user(user)
-            return redirect(url_for("index"))
+            return redirect(url_for("blackjack"))
 
         error = "Benutzername oder Passwort ist falsch."
 
